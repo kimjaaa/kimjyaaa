acbad55b2e73ad7a7f4555fcf8d644c96f4d49df
kimjaaa <nanjeesu@gmail.com>
2016-11-21 21:41:26 +0900

    first commit

---
+++ src/main/java/kimjyaaa/KimjyaaaApplication.java
@@ -0,0 +1,12 @@
+package kimjyaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimjyaaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimjyaaaApplication.class, args);
+	}
+}
---
+++ src/test/java/kimjyaaa/KimjyaaaApplicationTests.java
@@ -0,0 +1,16 @@
+package kimjyaaa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KimjyaaaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
